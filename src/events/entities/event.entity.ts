930957bbe98286474b1d6216d42d17f2273e87ca
Allison Colyer <allison.colyer@sweetgreen.com>
2024-01-30 17:35:55 -0800

    add indexes to event entity

--- src/events/entities/event.entity.ts
+++ src/events/entities/event.entity.ts
@@ -1,9 +1,11 @@
 // A database transaction symbolizes a unit of work performed within a database management system
 // Transations are a reliable way to handle multipel tasks independent of other tasks
-// The QueryRunner class  is used for this
+// The QueryRunner class  is used for this. This ensure the ACID properties of our database.
 
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, Index, PrimaryGeneratedColumn } from 'typeorm';
 
+// Composite index that contains Multiple columns
+@Index(['name', 'type']) // <--
 @Entity()
 export class Event {
   @PrimaryGeneratedColumn()
@@ -12,6 +14,12 @@ export class Event {
   @Column()
   type: string;
 
+  // Special lookup tables that our database can use to speed up retrival.
+  // Let's say that a very common lookup is to retrieve an event based on its name.
+  // To help speed up this search, lets index the table based on it's name.
+  // Indexes can help give our application rapid random lookups and efficent access of order records
+  // Use them when performace is important
+  @Index()
   @Column()
   name: string;
 
